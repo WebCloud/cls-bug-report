f26ac6152d73e7c74100b45bfe5a777905fc9284
Glitch (glitch-hello-website) <none>
2022-02-21 11:14:58 +0000

    🎩🛋 Checkpoint ./index.html:15866025/284079 ./script.js:15866025/1489

---
+++ script.js
@@ -0,0 +1,22 @@
+const bottomSentinel = document.querySelector('.qnYVzttodnzg9WdrVQ1p');
+const bottomSentinelFirstChild = document.querySelector('.qnYVzttodnzg9WdrVQ1p > div:first-child');
+const bottomSentinelSecondChild = document.querySelector('.IV3eOwLiJ909txXT98gs');
+
+let iterations = 0;
+
+const interval = setInterval(() => {
+  if (iterations === 0) {
+    bottomSentinel.style.height = "7844px"
+    bottomSentinelFirstChild.style.height = "0px";
+    bottomSentinelSecondChild.style.height = "calc(100% - 0px);";
+  } else {
+    bottomSentinel.style.height = "5846px"
+    bottomSentinelFirstChild.style.height = "962px";
+    bottomSentinelSecondChild.style.height = "calc(100% - 962px);";
+  }
+  iterations++;
+}, 400);
+
+if (iterations === 2) {
+  clearInterval(interval);
+}
